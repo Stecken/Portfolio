c267ca535d2a3dc9e227704db1fb8113c51d3ef5
aa <aaaa>
2021-02-15 15:38:32 -0300

    Portfólio v1

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+var options = {
+    strings: ['<strong>Web</strong>', 'de <strong>Embarcados</strong>', 'de <strong>Softwares Desktop</strong>'],
+    typeSpeed: 40,
+    smartBackspace: true,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|'
+};
+
+var typed = new Typed('#auto-texto', options);
