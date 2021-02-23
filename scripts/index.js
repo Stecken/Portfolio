c012de3b77cb9ac68af899778437f63101f98fb4
aa <aaaa>
2021-02-23 13:30:48 -0300

    Portfolio v2

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,16 @@ var options = {
 };
 
 var typed = new Typed('#auto-texto', options);
+
+
+const registraEventMouse = (elemRecebeEvent, queryAnim) => {
+    let elem = window.document.querySelector(elemRecebeEvent);
+    let elemAnim = window.document.querySelector(queryAnim);
+    elem.addEventListener('mouseenter', () => {
+        elemAnim.style.maxWidth = '100%';
+        console.log('funcionou');
+    })
+    elem.addEventListener('mouseleave', () => {
+        elemAnim.style.maxWidth = '0';
+    })
+}
